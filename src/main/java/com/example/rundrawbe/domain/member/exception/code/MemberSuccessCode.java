e7310ce4e8bf259e4b9bff5a4f6a8ead6c5d7ec5
Anna0626 <lsr00626@naver.com>
2026-07-23 01:24:21 +0900

    feat: 닉네임 설정 구현

--- src/main/java/com/example/rundrawbe/domain/member/exception/code/MemberSuccessCode.java
+++ src/main/java/com/example/rundrawbe/domain/member/exception/code/MemberSuccessCode.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpStatus;
 public enum MemberSuccessCode implements BaseSuccessCode {
 
     MEMBER_SET_SUCCESS(HttpStatus.OK, "MEMBER200_1", "성공적으로 유저를 조회했습니다."),
+    MEMBER_UPDATE_SUCCESS(HttpStatus.OK, "MEMBER200_2", "성공적으로 유저를 수정했습니다."),
     OK(HttpStatus.OK, "AUTH200_1", "로그인에 성공했습니다.");
 
     private final HttpStatus status;
     private final String code;
     private final String message;
+
 }
--- src/main/java/com/example/rundrawbe/domain/member/service/MemberService.java
+++ src/main/java/com/example/rundrawbe/domain/member/service/MemberService.java
@@ -1,5 +1,10 @@
 package com.example.rundrawbe.domain.member.service;
 
+import com.example.rundrawbe.domain.member.dto.MemberReqDTO;
+import com.example.rundrawbe.domain.member.entity.Member;
+import com.example.rundrawbe.domain.member.exception.MemberException;
+import com.example.rundrawbe.domain.member.exception.code.MemberErrorCode;
+import com.example.rundrawbe.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 닉네임 설정
+    public String updateName(Long memberId, MemberReqDTO.nickname dto) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+        if(memberRepository.existsByName(dto.nickname())){
+            throw new MemberException(MemberErrorCode.MEMBER_NAME_DUPLICATE);
+        }
+        member.updateName(dto.nickname());
+        return dto.nickname();
+    }
 }
